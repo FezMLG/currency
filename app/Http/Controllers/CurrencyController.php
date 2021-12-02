77bf7745485194586dcd3aa749406d9b518e93d4
FezMLG <46359181+FezMLG@users.noreply.github.com>
2021-12-02 18:37:44 +0100

    getting data from nbp api

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Currency;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CurrencyController extends Controller
 {
     public function show() {
         $currencies = Currency::all();
         return view('welcome', compact('currencies'));
-        
+    }
+
+    public function update() {
+
+        $url = "https://api.nbp.pl/api/exchangerates/tables/A/?format=json";
+
+        $response = Http::get($url);
+        $response->json();
+        $output = $response['0']['rates'];
+
+        foreach ($output as $key) {
+            var_dump($key["currency"]);
+        }
+                
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Currency App</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -47,6 +47,10 @@
                         @endforeach
                     </div>
                 </div>
+                <form action="/update" method="GET">
+                    @csrf
+                    <button>Update Currencies</button>
+                </form>
             </div>
         </div>
     </body>
