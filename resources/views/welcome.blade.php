650e4698fbc090384f801ca9e0225a8720f86d09
FezMLG <46359181+FezMLG@users.noreply.github.com>
2021-12-10 11:22:26 +0100

    update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,8 +49,13 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <h1 class="text-gray-900 dark:text-white">Waluty</h1>
+                  @if (session('status'))
+                    <div class="text-gray-900 dark:text-white">
+                      {{ session('status') }}
+                    </div>
+                  @endif
                 </div>
-
+            
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid">
                         @foreach ($currencies as $item)
